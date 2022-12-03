f096f75975776aaa2ce5e6a0970407dfcc13349e
unknown <chamodkanchuka826@gmail.com>
2022-12-03 12:44:01 +0530

    create sign up form

--- src/pages/authentication/forms/Form.jsx
+++ src/pages/authentication/forms/Form.jsx
@@ -28,7 +28,59 @@ export default function Form() {
             </form>
 
             <form name='signUpForm'>
-                {/* Sign up hear */}
+                <div className="firstAndLastName">
+                    <div className="input">
+                        <label htmlFor="signUpFirstName">First Name</label>
+                        <input type="text" placeholder='Enter First Name' id='signUpFirstName' />
+                        <span className='error_message'>Error</span>
+                    </div>
+
+                    <div className="input">
+                        <label htmlFor="signUpLastName">Last Name</label>
+                        <input type="text" placeholder='Enter Password' id='signUpLastName' />
+                        <span className='error_message'>Error</span>
+                    </div>
+                </div>
+                
+                <div className="input">
+                    <label htmlFor="signUpUseName">User Name</label>
+                    <input type="text" placeholder='Enter User Name' id='signUpUseName' />
+                    <span className='error_message'>Error</span>
+                </div>
+                
+                <div className="input">
+                    <label htmlFor="signUpMobile">Mobile Number</label>
+                    <input type="mobile" placeholder='Enter Mobile Number' id='signUpMobile' />
+                    <span className='error_message'>Error</span>
+                </div>
+                
+                <div className="input">
+                    <label htmlFor="signEmail">Email</label>
+                    <input type="email" placeholder='Enter Email Address' id='signEmail' />
+                    <span className='error_message'>Error</span>
+                </div>
+                
+                <div className="passwordAndRePassword">
+                    <div className="input">
+                        <label htmlFor="firstPassword">Password</label>
+                        <input type="password" placeholder='Enter Password' id='firstPassword' />
+                        <span className='error_message'>Error</span>
+                    </div>
+                    
+                    <div className="input">
+                        <label htmlFor="secondPassword">Confirm Password</label>
+                        <input type="password" placeholder='Confirm Password' id='secondPassword' />
+                        <span className='error_message'>Error</span>
+                    </div>
+                </div>
+
+                <div style={{padding:'5px'}}>
+                <input type="checkbox" id="check-box" /> <label htmlFor="check-box" style={{fontSize: '15px', fontWeight: "500"}}>I agreed with all terms & conditions</label>
+                </div>
+
+                <button>Register</button>
+
+                <p>If you have an account, <span onClick={()=>setChoseMethod(true)}>Login</span></p>
             </form>
         </div>
 
