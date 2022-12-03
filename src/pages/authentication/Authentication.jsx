5aa539b9999511cf3717e768534aeb2e178f80a8
ushan <ushanchamodbandara@gmail.com>
2022-12-03 14:25:18 +0530

    bug fix

--- src/pages/authentication/Authentication.jsx
+++ src/pages/authentication/Authentication.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AuthenticationNav from './authentication_nav/Authentication_nav';
+import SlideShow from './slide_show/SlideShow';
+import Form from './forms/Form';
 import './authentication.scss';
 
 export default function Authentication() {
@@ -7,7 +9,17 @@ export default function Authentication() {
     <>
         <AuthenticationNav />
         <div className='authentication_container'>
-            
+            <div className="content">
+
+              <div className="image_slide_show">
+                <SlideShow />
+              </div>
+
+              <div className="authentication_form">
+                <Form />
+              </div>
+
+            </div>
         </div>
     </>
   )
