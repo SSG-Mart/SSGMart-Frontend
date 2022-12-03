005f2bee1f701786e09d4d0e04c2a92e3b1952d9
ushan <ushanchamodbandara@gmail.com>
2022-12-03 13:52:10 +0530

    create image slider and fix all bugs

--- src/pages/authentication/Authentication.jsx
+++ src/pages/authentication/Authentication.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AuthenticationNav from './authentication_nav/Authentication_nav';
 import SlideShow from './slide_show/SlideShow';
 import Form from './forms/Form';
 import './authentication.scss';
@@ -6,6 +7,7 @@ import './authentication.scss';
 export default function Authentication() {
   return (
     <>
+        <AuthenticationNav />
         <div className='authentication_container'>
             <div className="content">
 
