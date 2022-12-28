746e03bb547e6d989af357e86d55916e333655f9
TharinduNilan <tharindunilan04@gmail.com>
2022-12-29 00:11:07 +0530

    Added the api to the add item

--- src/components/addItem/addItem.jsx
+++ src/components/addItem/addItem.jsx
@@ -1,28 +1,60 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 import { AiFillCloseCircle} from 'react-icons/ai';
-import './addItem.scss'
+import './addItem.scss';
 
 export default function AddItem() {
     const [title,setTitle] = useState ("")
     const [quantity,setQuantity] = useState ("")
     const [unit,setUnit] = useState ("")
-    const [subCatagory,setSubCatagory] = useState ("")
+    const [catagory, setCategory] = useState ("")
+    const [subcategory,setSubcategory] = useState ("")
     const [unitPrice,setUnitPrice] = useState ("")
     const [image,setImage] = useState ("")
-    const [discription,setDiscription] = useState ("")
+    const [description,setDescription] = useState ("")
     const [radio,setRadio]=useState();
     const [expireDate, setExpireDate] = useState(1)
+    const [massage, setMassage] = useState ("")
 
     const [error,setError] = useState(false)
 
-    const handleSubmit=(e)=>{
+    //Main Category and sub category
+    const [api_category, set_api_category] = useState()
+
+
+    useEffect(() => {
+        const getCategory = async() => {
+            try{
+                var res = await axios.post("api/additem/getcategory");
+                set_api_category(res.data);
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
+
+        getCategory();
+        console.log(api_category);
+    },[])
+    
+    
+    const handleSubmit= async (e)=>{
         e.preventDefault();
-        if(title.length===0||quantity.length===0||unit.length===0||subCatagory.length===0||unitPrice.length===0||image.length||expireDate.length){
-            setError(true)
-        }
-        if (title&&quantity&&unit&&subCatagory&&unitPrice&&discription&&image) {
-            console.log(title,quantity,radio,subCatagory,unitPrice,discription,expireDate,image)
-        }
+        // if(title.length===0||quantity.length===0||unit.length===0||subcategory.length===0||unitPrice.length===0||image.length||expireDate.length){
+        //     setError(true)
+        // }
+        // if (title&&quantity&&unit&&subcategory&&unitPrice&&description&&image) {
+        //     // console.log(title,quantity,radio,subcategory,unitPrice,description,expireDate,image,catagory)
+        
+        //     const userdata={title:title, quantity:quantity, sub_category_id:subcategory, unit_price:unitPrice, description:description, time_period:expireDate, image:"image", category_id:"1", unit:unit};
+        //     // console.log(userdata)
+        //     await axios.post("/api/additem",{userdata})
+        
+
+        // .then (result=>{ setMassage(result.data.msg); console.log(result.data)});
+        // }
+
+        console.log(api_category);
     }
 
   return (
@@ -41,7 +73,7 @@ export default function AddItem() {
         </div>
 
         {/* Main Table */}
-        <div className='table-devision'>
+        <div className='table-decision'>
         <form onSubmit={handleSubmit}>
         <table border={0}>
             <tbody>
@@ -50,7 +82,7 @@ export default function AddItem() {
                     <div className='title'>
                         <p>Title</p>
                         <input type='text' placeholder='Enter Title'
-                        onChange={e=>setTitle(e.target.value)}
+                        onChange={(e)=>setTitle(e.target.value)}
                         />
                         <br />
                         {error && title.length<=0?
@@ -58,39 +90,39 @@ export default function AddItem() {
                     </div>
                 </td>
                 <td>
-                    <div className='unit' onChange={e=>setUnit(e.target.value)}>
+                    <div className='unit' onChange={(e)=>setUnit(e.target.value)}>
                         <p>Unit</p>
-                        <label className='radio'><input type="radio" name="radio" value="Kilogram" onChange={e=>setRadio(e.target.value)}/>Kilogram</label>
-                        <label className='radio'><input type="radio" name="radio" value="Liter" onChange={e=>setRadio(e.target.value)}/>Liter</label>
-                        <label className='radio'><input type="radio" name="radio" value="Pieces" onChange={e=>setRadio(e.target.value)}/>Pieces</label>
+                        <label className='radio'><input type="radio" name="radio" value="Kilogram" onChange={(e)=>setRadio(e.target.value)}/>Kilogram</label>
+                        <label className='radio'><input type="radio" name="radio" value="Liter" onChange={(e)=>setRadio(e.target.value)}/>Liter</label>
+                        <label className='radio'><input type="radio" name="radio" value="Pieces" onChange={(e)=>setRadio(e.target.value)}/>Pieces</label>
                         <br />
                         {error && unit.length<=0?
-                        <label>requred</label>:""}
+                        <label>required</label>:""}
                     </div>
                 </td>
             </tr>
             <tr>
                 <td>
-                <div className='Catagory'>
-                        <p>Catagory</p>
-                        <select name="catagory" id="catagory" disabled>
-                            <option value="select 1">select 1</option>
-                            <option value="select 2">select 2</option>
+                    <div className='category'>
+                        <p>category</p>
+                        <select name="category" id="category" disabled onChange={(e)=>setCategory(e.target.value)}>
+                            <option value="select 1" id='1'>select 1</option>
+                            <option value="select 2" id='2'>select 2</option>
                         </select>
                     </div>
                 </td>
                 <td>
-                    <div className='Sub-Catagory'>
-                        <p>Sub Catagory</p>
-                        <select name="sub-catagory" id="catagory" onChange={e=>setSubCatagory(e.target.value)}>
-                            <option value="Sub 1" >Sub-Catagory 1</option>
-                            <option value="Sub 2" >Sub-Catagory 2</option>
-                            <option value="Sub 3" >Sub-Catagory 3</option>
-                            <option value="Sub 4" >Sub-Catagory 4</option>
+                    <div className='Sub-category'>
+                        <p>Sub category</p>
+                        <select name="sub-category" id="category" onChange={(e)=>setSubcategory(e.target.value)}>
+                            <option value="Sub 1" >Sub-category 1</option>
+                            <option value="Sub 2" >Sub-category 2</option>
+                            <option value="Sub 3" >Sub-category 3</option>
+                            <option value="Sub 4" >Sub-category 4</option>
                         </select>
                         <br />
-                        {error && subCatagory.length<=0?
-                        <label>The sub catagory must be select</label> :""}
+                        {error && subcategory.length<=0?
+                        <label>The sub category must be select</label> :""}
                     </div>
                 </td>
             </tr>
@@ -110,7 +142,7 @@ export default function AddItem() {
                     <div className='unit-price'>
                         <p>Unit Price</p>
                         <input className='unit-input' type='text' placeholder='Enter unit price'
-                        onChange={e=>setUnitPrice(e.target.value)}
+                        onChange={(e)=>setUnitPrice(e.target.value)}
                         />
                         <br />
                         {error && unitPrice.length<=0?
@@ -123,7 +155,7 @@ export default function AddItem() {
                     <div className='add-img'>
                         <p>Add Image</p>
                         <input type="file" multiple accept="image/*"
-                        onChange={e=>setImage(e.target.value)}
+                        onChange={(e)=>setImage(e.target.value)}
                         />
                         <br />
                         {error && image.length<=0?
@@ -134,30 +166,32 @@ export default function AddItem() {
                     <div className='expire-date'>
                         <p>Expire Date</p>
                         <input type='number' value={expireDate} placeholder='Enter Expire Date' min={1}
-                        onChange={e=>setExpireDate(e.target.value)}/>
+                        onChange={(e)=>setExpireDate(e.target.value)}/>
                     </div>
                 </td>
             </tr>
             <tr>
                 <td>
-                    <div className='Discription'>
-                        <p>Discription</p>
-                        <textarea type='text' rows="4" cols="26" placeholder='Enter Your Discription'
-                        onChange={e=>setDiscription(e.target.value)}
+                    <div className='Description'>
+                        <p>Description</p>
+                        <textarea type='text' rows="4" cols="26" placeholder='Enter Your Description'
+                        onChange={(e)=>setDescription(e.target.value)}
                         />
                         <br />
-                        {error && discription.length<=0?
-                        <label>The discription can't be empty</label> :""}
+                        {error && description.length<=0?
+                        <label>The description can't be empty</label> :""}
                     </div>
                 </td>
-                <td></td>
+                <td>
+                    <div className='submit'>
+                        <input type="submit" name='submit' value="List Item" />
+                    </div>
+                </td>
             </tr>
             <tr>
                 <td></td>
                 <td>
-                    <div className='submit'>
-                        <input type="submit" value="List Item"/>
-                    </div>
+
                 </td>
             </tr>
             </tbody>
