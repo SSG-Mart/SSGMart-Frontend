69785287ea572ab77cb56d461b16576532ba28b4
Kanchuka Chamod <91131568+Chamod826@users.noreply.github.com>
2022-12-29 18:09:42 +0530

    Rebuild Seller Dashboard Adjustments

--- src/pages/Sellers_dashboard/selldb.jsx
+++ src/pages/Sellers_dashboard/selldb.jsx
@@ -7,251 +7,46 @@ function selldb() {
   return (
     <div className="main_seldb">
       <div className="names">
-        <img
-          className="imlg"
-          src={Imlg}
-          alt="Imgagein"
-          width={70}
-          height={70}
-        />
+        <img className="imlg" src={Imlg} alt="Imgagein" />
         <h1>SSGMart</h1>
       </div>
 
       <div className="plus">
-        <FaPlus size={30} />
+        <FaPlus className="Plus_Button" />
       </div>
       <div className="back">
         <div className="After_Back">
-          <div className="card">
-            <div className="backgt1">
-              <div className="text1">No</div>
-            </div>
-            <div className="backgt2">
-              <div className="text2">Image</div>
-            </div>
-            <div className="backgt3">
-              <div className="text3">Discription</div>
-            </div>
-            <div className="backgt4">
-              <div className="text4">ABC</div>
-            </div>
-          </div>
-          <div className="c1">
-            <div className="backgt1">
-              <div className="text1">01</div>
-            </div>
-            <div className="backgt2">
-              <div className="text2"></div>
-            </div>
-            <div className="backgt3">
-              <div className="text3">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
-              </div>
-            </div>
-            <div className="backgt4">
-              <div className="text4">
-                <div className="fst">
-                  <div className="g">Add discount</div>
-                  <div className="o">Preview</div>
-                </div>
-                <div className="snd">
-                  <div className="r">Remove Item</div>
-                  <div className="b">Edit</div>
-                </div>
-              </div>
-            </div>
-            <div className="c2">
-              <div className="backgt1">
-                <div className="text1">02</div>
-              </div>
-              <div className="backgt2">
-                <div className="text2"></div>
-              </div>
-              <div className="backgt3">
-                <div className="text3">
+          <table className="table_one">
+            <thead>
+              <tr>
+                <th className="Topic_No">No</th>
+                <th className="Topic_Image">Image</th>
+                <th className="Topic_Discription">Discription</th>
+                <th className="Topic_ABC">ABC</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="Body_tr">
+                <td className="Data_No">01</td>
+                <td className="Data_Image"></td>
+                <td className="Data_Discription">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the industry's
                   standard dummy text ever since the 1500s,
-                </div>
-              </div>
-              <div className="backgt4">
-                <div className="text4">
-                  <div className="fst">
-                    <div className="g">Add discount</div>
-                    <div className="o">Preview</div>
-                  </div>
-                  <div className="snd">
-                    <div className="r">Remove Item</div>
-                    <div className="b">Edit</div>
-                  </div>
-                </div>
-              </div>
-              <div className="c3">
-                <div className="backgt1">
-                  <div className="text1">03</div>
-                </div>
-                <div className="backgt2">
-                  <div className="text2"></div>
-                </div>
-                <div className="backgt3">
-                  <div className="text3">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s,
-                  </div>
-                </div>
-                <div className="backgt4">
-                  <div className="text4">
-                    <div className="fst">
-                      <div className="g">Add discount</div>
-                      <div className="o">Preview</div>
+                </td>
+                <td className="Data_ABC">
+                    <div className="fst_section">
+                      <button className="Green">Add Discount</button>
+                      <button className="Orange">Preview</button>
                     </div>
-                    <div className="snd">
-                      <div className="r">Remove Item</div>
-                      <div className="b">Edit</div>
+                    <div className="snd_section">
+                      <button className="Red">Remove Item</button>
+                      <button className="Blue">Edit</button>
                     </div>
-                  </div>
-                </div>
-                <div className="c4">
-                  <div className="backgt1">
-                    <div className="text1">04</div>
-                  </div>
-                  <div className="backgt2">
-                    <div className="text2"></div>
-                  </div>
-                  <div className="backgt3">
-                    <div className="text3">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s,
-                    </div>
-                  </div>
-                  <div className="backgt4">
-                    <div className="text4">
-                      <div className="fst">
-                        <div className="g">Add discount</div>
-                        <div className="o">Preview</div>
-                      </div>
-                      <div className="snd">
-                        <div className="r">Remove Item</div>
-                        <div className="b">Edit</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="c5">
-                    <div className="backgt1">
-                      <div className="text1">05</div>
-                    </div>
-                    <div className="backgt2">
-                      <div className="text2"></div>
-                    </div>
-                    <div className="backgt3">
-                      <div className="text3">
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                      </div>
-                    </div>
-                    <div className="backgt4">
-                      <div className="text4">
-                        <div className="fst">
-                          <div className="g">Add discount</div>
-                          <div className="o">Preview</div>
-                        </div>
-                        <div className="snd">
-                          <div className="r">Remove Item</div>
-                          <div className="b">Edit</div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="c6">
-                      <div className="backgt1">
-                        <div className="text1">06</div>
-                      </div>
-                      <div className="backgt2">
-                        <div className="text2"></div>
-                      </div>
-                      <div className="backgt3">
-                        <div className="text3">
-                          Lorem Ipsum is simply dummy text of the printing and
-                          typesetting industry. Lorem Ipsum has been the
-                          industry's standard dummy text ever since the 1500s,
-                        </div>
-                      </div>
-                      <div className="backgt4">
-                        <div className="text4">
-                          <div className="fst">
-                            <div className="g">Add discount</div>
-                            <div className="o">Preview</div>
-                          </div>
-                          <div className="snd">
-                            <div className="r">Remove Item</div>
-                            <div className="b">Edit</div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="c7">
-                        <div className="backgt1">
-                          <div className="text1">07</div>
-                        </div>
-                        <div className="backgt2">
-                          <div className="text2"></div>
-                        </div>
-                        <div className="backgt3">
-                          <div className="text3">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s,
-                          </div>
-                        </div>
-                        <div className="backgt4">
-                          <div className="text4">
-                            <div className="fst">
-                              <div className="g">Add discount</div>
-                              <div className="o">Preview</div>
-                            </div>
-                            <div className="snd">
-                              <div className="r">Remove Item</div>
-                              <div className="b">Edit</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="c8">
-                        <div className="backgt1">
-                          <div className="text1">08</div>
-                        </div>
-                        <div className="backgt2">
-                          <div className="text2"></div>
-                        </div>
-                        <div className="backgt3">
-                          <div className="text3">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s,
-                          </div>
-                        </div>
-                        <div className="backgt4">
-                          <div className="text4">
-                            <div className="fst">
-                              <div className="g">Add discount</div>
-                              <div className="o">Preview</div>
-                            </div>
-                            <div className="snd">
-                              <div className="r">Remove Item</div>
-                              <div className="b">Edit</div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
