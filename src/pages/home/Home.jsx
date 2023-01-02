b47b83fe2f0a6627a004db3fe27a5071da42a207
ushan <ushanchamodbandara@gmail.com>
2023-01-02 23:09:10 +0530

    Add pop-up to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import Sidebar from "../navBar/Sidebar";
 import SearchBar from "./searchBar/searchBar";
 import HomePage from "./HomePage/homePage";
 import "./Home.scss";
+import PopUp from "../../components/popup-item/popup-i"
 
 export default function Home() {
+  const [data, setData] = useState({});
+  const [popUpState, setPopUpState] = useState(false);
+  
+  const clickClose = () => {
+    setPopUpState(false);
+  }
+  
+  const showPopUp = (e) => {
+    setData(e);
+    setPopUpState(true);
+  }
+
+  const setPopUpData = e => {
+    let data = {
+      item_name: e.name,
+      description: e.description,
+      unit_price: e.unit_price,
+      unit: e.unit,
+      store_name: e.store_name,
+      city: e.district_name,
+      mobile:e.mobile,
+      item_image:e.image,
+      user_image:e.user_image
+    }
+    showPopUp(data);
+  }
+
+  
+
   return (
+    <>
+    <div style={popUpState ? null : {display: 'none'}}>
+      <PopUp popUpData = {data} clickClose={clickClose} />
+    </div>
     <div className="home-container">
       <Sidebar />
       <SearchBar />
-      <HomePage />
+      <HomePage setPopUpData={setPopUpData} showPopUp={showPopUp} />
     </div>
+    </>
   );
 }
