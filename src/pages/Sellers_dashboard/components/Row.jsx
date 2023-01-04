f71539e2542b5e131ea91b52bb01a5d2ae80b70c
ushan <ushanchamodbandara@gmail.com>
2023-01-04 15:08:04 +0530

    create functionality od preview item and remove item

---
+++ src/pages/Sellers_dashboard/components/Row.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+// import item_image from "../ss.jpeg";
+import axios from "axios";
+
+const Row = (props) => {
+  console.log(props.item);
+
+  //add discount
+  // preview
+  function showPreview() {
+    const data = {
+      item_name: props.item.name,
+      description: props.item.description,
+      unit_price: props.item.unit_price,
+      unit: props.item.unit,
+      store_name: props.item.store_name
+      ,
+      city: props.item.city_name,
+      mobile: props.item.mobile,
+      item_image: props.item.image,
+      user_image: props.item.user_image,
+    }
+    props.showPreview(data);
+  }
+
+
+  // remove item
+  async function removeItem(){
+    var res = await axios.post("/api/cruditem/delete", { store_id: props.item.seller_id, item_id: props.item.item_id});
+    // setData(res.data);
+    console.log(res.data);
+    props.trigger(!props.triggerValue)
+  }
+  
+  // edit item
+  return (
+    <>
+
+      <div className="detail-body">
+        <div className="Data_No">{props.item.item_id}</div>
+        <div className="Data_Image">
+          <img src={`http://localhost:8080/api/img/item/${props.item.image}`} alt="Item" />
+        </div>
+        <div className="Data_Discription">
+          {props.item.description}
+        </div>
+        <div className="button-section">
+          <div className="fst_section">
+            <label className="add-btn">Add Discount</label>
+            <label className="preview-btn" onClick={showPreview}>Preview</label>
+          </div>
+          <div className="snd_section">
+            <label className="Remove-btn" onClick={removeItem}>Remove Item</label>
+            <label className="edit-btn">Edit</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
--- src/pages/Sellers_dashboard/selldb.jsx
+++ src/pages/Sellers_dashboard/selldb.jsx
@@ -1,222 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AddItem from "../../components/addItem/addItem";
+import Itempopup from "../../components/popup-item/popup-i";
 import "./selldb.scss";
 import Logo from "./ssg_mart.png";
-import item_image from "./ss.jpeg";
 import { FaPlus } from "react-icons/fa";
 
-function selldb() {
+import Row from "./components/Row";
+
+function Selldb() {
+  const [toggleAddItem, setToggleAddItem] = useState(false);
+  const [data, setData] = useState();
+  const [trigger, setTrigger] = useState();
+  const [popUpData, setPopUpData] = useState([]);
+  const [preview, setPreview] = useState(false);
+
+
+  useEffect(() => {
+    // axios async call
+    async function fetchData() {
+      var res = await axios.post("/api/dashboard");
+      setData(res.data);
+    }
+    fetchData();
+    console.log(data);
+    // eslint-disable-next-line
+  }, [trigger]);
+
+  //close popup preview
+  function clickClose() {
+    setPreview(false)
+  }
+  function showPreview(e) {
+    console.log(e);
+    setPopUpData(e)
+    setPreview(true)
+  }
+
   return (
-    <div className="main_seldb">
-      <div className="names">
-      <div className="header-section">
-
+    <>
+    <div style={preview ? null : {display:'none'}}>
+      <Itempopup popUpData = {popUpData} clickClose={clickClose} />
+    </div>
+      <div
+        className="add_item"
+        style={toggleAddItem ? null : { display: "none" }}
+      >
+        <AddItem toggleAdd={setToggleAddItem} />
+      </div>
+      <div className="main_seldb">
         <div className="names">
-          <img className="logo" src={Logo} alt="logo" />
+          <div className="header-section">
+            <div className="names">
+              <img className="logo" src={Logo} alt="logo" />
+            </div>
+            <h1>SSG Mart</h1>
+          </div>
         </div>
-          <h1>SSG Mart</h1>
-        </div>
-      </div>
 
-      <div className="main-body-section">
-      <div className="plus">
-        <FaPlus className="Plus_Button" />
-      </div>
-        <div className="dashbord-card">
-
+        <div className="main-body-section">
+          <div className="plus">
+            <FaPlus
+              className="Plus_Button"
+              onClick={() => setToggleAddItem(true)}
+            />
+          </div>
+          <div className="dashbord-card">
             <div className="header-section">
-
               <div className="table-hrader">
                 <div className="headre-data">
-                  <span className="Topic_No">No</span>
+                  <span className="Topic_No">ID</span>
                   <span className="Topic_Image">Image</span>
                   <span className="Topic_Discription">Discription</span>
                   <span className="Topic_ABC">ABC</span>
                 </div>
               </div>
-
             </div>
 
             <div className="body-section">
+              {
+                typeof(data) === 'undefined' ? null :
+                data.map((item, index) => {
+                  return <Row key={index} item={item} trigger={setTrigger} triggerValue={trigger} showPreview={showPreview} />
+                })
+              }
 
 
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-              <div className="detail-body">
-                <div className="Data_No">01</div>
-                <div className="Data_Image"><img src={item_image} alt='Item'/></div>
-                <div className="Data_Discription">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s,
-                </div>
-                <div className="button-section">
-                    <div className="fst_section">
-                      <label className="add-btn">Add Discount</label>
-                      <label className="preview-btn">Preview</label>
-                    </div>
-                    <div className="snd_section">
-                      <label className="Remove-btn">Remove Item</label>
-                      <label className="edit-btn">Edit</label>
-                    </div>
-                </div>
-              </div>
-
-
+              {/* 
+              Item Image
+              Item Discription
+              add discount API
+              remove Item API
+              edit Item API
+              preview API
+              */}
+            </div>
+            {/* body-section */}
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default selldb;
+export default Selldb;
