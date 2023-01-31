13187404cb89dd6d88814ea32032476b9ebde298
ushanchamod <ushanchamodbandara@gmail.com>
2023-01-31 13:28:12 +0530

    modified:   src/App.js 	modified:   src/index.js 	modified:   src/pages/navBar/Nav.jsx 	new file:   src/pages/user_profile/User_profile_view.jsx 	new file:   src/pages/user_profile/sec_two.jsx 	new file:   src/pages/user_profile/seller_view.scss

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import Catagory from "./pages/CatagoryPage/catagoryHome";
 import About from "./pages/AboutUs/about";
 import Contact from "./pages/Contactus/contact";
 
+import UserProfile from "./pages/user_profile/User_profile_view";
+
 
 function App() {
   const [isAuth, setIsAuth] = useState(404);
@@ -63,9 +65,10 @@ function App() {
             <Route path="/store/:store_name" element={<SellerView />} />
             <Route path="additem" element={<AddItem />} />
 
-
-          <Route path="*" element={<NotFound />} />
           <Route path="/catagory" element={<Catagory />} />
+          
+          <Route path="/profile" element={<UserProfile />} />
+
 
             <Route
               path="/auth"
